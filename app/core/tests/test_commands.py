68867ceed7ce91d15a68891c177113dd10804326
unknown <pittnerf@gmail.com>
2023-03-17 17:17:46 +0100

    update checks

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,7 +3,7 @@ Test custom Django
 """
 
 from unittest.mock import patch
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -13,22 +13,22 @@ from django.test import SimpleTestCase
 class CommandTests(SimpleTestCase):
     """Test commands"""
 
-    def test_for_db_ready(self, patched_check):
+    def test_wait_for_db_ready(self, patched_check):
         """Test waitng for db"""
         patched_check.return_value = True
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """ Test waiting for Db when getting operational error"""
 
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
         [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
-        patched_check.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
