ce6b583665cc83db76658f8c23ed6f1b391994dd
unknown <pittnerf@gmail.com>
2023-03-21 11:48:46 +0100

    update code 230321_1

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -13,7 +13,7 @@ from recipe import serializers
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs"""
 
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -22,3 +22,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Retrieve the recipes for authenticated user -
         must be overriden to filter it only for auth user"""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return the serializer class for request used for details view.
+        So all endpoints expect 'list' will use the DetailSerializer."""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
